92840610d837332c72979f32bafa9e6178ccabc5
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-12-03 17:06:15 +0000

    Update kmonad

--- kmonad/ducky.sh
+++ kmonad/ducky.sh
@@ -2,4 +2,4 @@
 
 export KB_PATH="/dev/input/by-id/usb-Ducky_Ducky_One_3_SF_RGB_DK-V1.07-220107-if01-event-kbd"
 KBDCFG=$(envsubst < conf.kbd)
-kmonad <(echo "$KBDCFG")
+kmonad <(echo "$KBDCFG") --log-level debug
