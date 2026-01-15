ad9e1926fe476879ae2417a8cd6243fd4a7462c7
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2026-01-15 18:26:51 +0100

    feat: update sway
    Signed-off-by: Jonatan Hanssen <jonatan.hanssen@outlook.com>

--- sway/trans.py
+++ sway/trans.py
@@ -7,7 +7,7 @@
 
 import i3ipc
 
-transparency_val = "0.95"
+transparency_val = '0.95'
 ipc = i3ipc.Connection()
 prev_focused = None
 
@@ -15,7 +15,7 @@ for window in ipc.get_tree():
     if window.focused:
         prev_focused = window
     else:
-        window.command("opacity " + transparency_val)
+        window.command('opacity ' + transparency_val)
 
 
 def on_window_focus(ipc, focused):
@@ -23,10 +23,10 @@ def on_window_focus(ipc, focused):
     if (
         focused.container.id != prev_focused.id
     ):  # https://github.com/swaywm/sway/issues/2859
-        focused.container.command("opacity 1")
-        prev_focused.command("opacity " + transparency_val)
+        focused.container.command('opacity 1')
+        prev_focused.command('opacity ' + transparency_val)
         prev_focused = focused.container
 
 
-ipc.on("window::focus", on_window_focus)
+ipc.on('window::focus', on_window_focus)
 ipc.main()
